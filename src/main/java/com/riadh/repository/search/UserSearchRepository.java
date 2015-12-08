3c7f7b405942197317ec44d0f07e6114dd58eacf
Riadh MNASRI <riadh.mnasri@yahoo.fr>
2015-12-08 13:03:43 +0100

    My generated JHipster app

---
+++ src/main/java/com/riadh/repository/search/UserSearchRepository.java
@@ -0,0 +1,10 @@
+package com.riadh.repository.search;
+
+import com.riadh.domain.User;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Spring Data ElasticSearch repository for the User entity.
+ */
+public interface UserSearchRepository extends ElasticsearchRepository<User, Long> {
+}
---
+++ src/main/java/com/riadh/web/rest/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects used by Spring MVC REST controllers.
+ */
+package com.riadh.web.rest.dto;
